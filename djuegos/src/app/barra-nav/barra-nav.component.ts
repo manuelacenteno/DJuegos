9cdeb0adb3bb6d2a6bd9a71bd5ea698c96e58af1
Manu <centenomanuela40@gmail.com>
2022-05-22 18:52:47 -0300

    barra de navegacion y footer

---
+++ djuegos/src/app/barra-nav/barra-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-nav',
+  templateUrl: './barra-nav.component.html',
+  styleUrls: ['./barra-nav.component.scss']
+})
+export class BarraNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
