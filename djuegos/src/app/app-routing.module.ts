9fd3dd9dd7d1f941a59b1201d28359bba3799e42
Manu <centenomanuela40@gmail.com>
2022-05-30 18:41:59 -0300

    arreglos

--- djuegos/src/app/app-routing.module.ts
+++ djuegos/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-// import { FormLoginComponent } from './form-login/form-login.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 
 const routes: Routes = [
