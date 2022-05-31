2113e65bcae0112ec22f888537c272b6293a6336
Manu <centenomanuela40@gmail.com>
2022-05-30 23:46:03 -0300

    agregue juego.ts, css y html del componente busqueda

--- djuegos/src/app/app-routing.module.ts
+++ djuegos/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormLoginComponent } from './form-login/form-login.component';
+import { JuegosListComponent } from './juegos-list/juegos-list.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: AppComponent,
+  },
+  {
+    path: 'busqueda',
+    component: JuegosListComponent,
   }
 ];
 
--- djuegos/src/app/busqueda/busqueda.component.ts
+++ djuegos/src/app/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Juego} from './juego';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,6 +7,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busqueda.component.scss']
 })
 export class BusquedaComponent implements OnInit {
+
+  juegos: Juego [] =
+  [{
+    id: 1,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 6,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 7,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 8,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 9,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 10,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  },
+  {
+    id: 11,
+    nombre: "Roblox",
+    tipo: " Aventura",
+    calificacion:3,
+    imagen: "assets/img/juegoAventura1.jpg"
+  }
+]
 
   constructor() { }
 
