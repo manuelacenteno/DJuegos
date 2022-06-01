96813ad586896c2680ffc9bfc42752fc26309c1a
Manu <centenomanuela40@gmail.com>
2022-06-01 00:26:53 -0300

    compontente busqueda lista

--- djuegos/src/app/pipes/filter.pipe.ts
+++ djuegos/src/app/pipes/filter.pipe.ts
@@ -6,15 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, ...arg: any[]): any {
-    if(arg.length > 5) return value; //si no se ingresan mas de 5 palabras no aparezca busqueda tambien se puede poner si esta vacio
+    
     const  resultadoBusqueda = [];
     for(const juego of value){
-      console.log(juego);
-      /*if(juego.nombre.toLowerCase().indexOf(arg)> -1){
+      if(juego.nombre.toLowerCase().indexOf(arg)> -1){
         resultadoBusqueda.push(juego);
-      }*/
+      }
     }
-    
+    return resultadoBusqueda;
   }
 
 }
